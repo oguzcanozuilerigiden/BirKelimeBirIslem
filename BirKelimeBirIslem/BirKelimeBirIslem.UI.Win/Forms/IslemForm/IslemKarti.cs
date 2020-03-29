da9b780695482286d74b05eacc24532d259d15ba
oguzcanozuilerigiden <62521629+oguzcanozuilerigiden@users.noreply.github.com>
2020-03-29 22:50:26 +0300

    Formlar İmplemente Edildi

---
+++ BirKelimeBirIslem/BirKelimeBirIslem.UI.Win/Forms/IslemForm/IslemKarti.cs
@@ -0,0 +1,12 @@
+﻿using BirKelimeBirIslem.UI.Win.Forms.Base;
+
+namespace BirKelimeBirIslem.UI.Win.Forms.IslemForm
+{
+    public partial class IslemKarti : BaseForm
+    {
+        public IslemKarti()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ BirKelimeBirIslem/BirKelimeBirIslem.UI.Win/Forms/KelimeForm/KelimeKarti.cs
@@ -0,0 +1,12 @@
+﻿using BirKelimeBirIslem.UI.Win.Forms.Base;
+
+namespace BirKelimeBirIslem.UI.Win.Forms.KelimeForm
+{
+    public partial class KelimeKarti : BaseForm
+    {
+        public KelimeKarti()
+        {
+            InitializeComponent();
+        }
+    }
+}
