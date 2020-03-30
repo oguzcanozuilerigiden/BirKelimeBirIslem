35d073ff0b45963fd111e842ad11b5b798043cce
oguzcanozuilerigiden <62521629+oguzcanozuilerigiden@users.noreply.github.com>
2020-03-30 20:19:21 +0300

    Master a MySimpleButton Eklendi

---
+++ BirKelimeBirIslem/BirKelimeBirIslem.UI.Win/UserControls/Controls/MySimpleButton.cs
@@ -0,0 +1,15 @@
+﻿using DevExpress.XtraEditors;
+using System.Drawing;
+using System.ComponentModel;
+
+namespace BirKelimeBirIslem.UI.Win.UserControls.Controls
+{
+    [ToolboxItem(true)]
+    public class MySimpleButton : SimpleButton
+    {
+        public MySimpleButton()
+        {
+            Appearance.ForeColor = Color.Maroon;
+        }
+    }
+}
